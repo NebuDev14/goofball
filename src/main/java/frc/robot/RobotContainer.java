9b351c98256f1deb3e67080d728475a56a500a38
warren yun <wyun2006@gmail.com>
2023-11-16 22:32:04 -0500

    init

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.AutonomousDistance;
+import frc.robot.commands.AutonomousTime;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.xrp.XRPOnBoardIO;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+public class RobotContainer {
+  private final Drivetrain m_drivetrain = new Drivetrain();
+  private final XRPOnBoardIO m_onboardIO = new XRPOnBoardIO();
+  private final Arm m_arm = new Arm();
+
+  private final Joystick left = new Joystick(0);
+  private final Joystick right = new Joystick(1);
+  private final Joystick buttons = new Joystick(2);
+
+  // Create SmartDashboard chooser for autonomous routines
+  private final SendableChooser<Command> m_chooser = new SendableChooser<>();
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  private void configureButtonBindings() {
+
+    m_drivetrain.setDefaultCommand(m_drivetrain.tankDrive(() -> left.getRawAxis(0), () -> right.getRawAxis(0)));
+
+    // Example of how to use the onboard IO
+    Trigger userButton = new Trigger(m_onboardIO::getUserButtonPressed);
+    userButton
+        .onTrue(new PrintCommand("USER Button Pressed"))
+        .onFalse(new PrintCommand("USER Button Released"));    
+
+    // Setup SmartDashboard options
+    m_chooser.setDefaultOption("Auto Routine Distance", new AutonomousDistance(m_drivetrain));
+    m_chooser.addOption("Auto Routine Time", new AutonomousTime(m_drivetrain));
+    SmartDashboard.putData(m_chooser);
+  }
+
+  public Command getAutonomousCommand() {
+    return m_chooser.getSelected();
+  }
+
+}
---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,202 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.function.DoubleSupplier;
+import com.pathplanner.lib.util.ReplanningConfig;
+import com.pathplanner.lib.commands.FollowPathRamsete;
+import com.pathplanner.lib.path.PathPlannerTrajectory;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.xrp.XRPGyro;
+import edu.wpi.first.wpilibj.xrp.XRPMotor;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.oblarg.oblog.annotations.Log;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.controller.LTVDifferentialDriveController;
+
+public class Drivetrain extends SubsystemBase {
+  private static final double kGearRatio = (30.0 / 14.0) * (28.0 / 16.0) * (36.0 / 9.0) * (26.0 / 8.0); // 48.75:1
+  private static final double kCountsPerMotorShaftRev = 12.0;
+  private static final double kCountsPerRevolution = kCountsPerMotorShaftRev * kGearRatio; // 585.0
+  private static final double kWheelDiameterInch = 2.3622; // 60 mm
+  private static final double kTrackWidth = 0;
+
+  private final XRPMotor m_leftMotor = new XRPMotor(0);
+  private final XRPMotor m_rightMotor = new XRPMotor(1);
+
+  private final Encoder m_leftEncoder = new Encoder(4, 5);
+  private final Encoder m_rightEncoder = new Encoder(6, 7);
+
+  // Set up the differential drive controller
+  private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
+  // Odometry
+  private final DifferentialDriveOdometry odometry;
+
+  // Kinematics
+  private final DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(kTrackWidth);
+
+  // Set up the XRPGyro
+  private final XRPGyro m_gyro = new XRPGyro();
+
+  // Set up the BuiltInAccelerometer
+  private final BuiltInAccelerometer m_accelerometer = new BuiltInAccelerometer();
+
+  // Feedforward
+  private final SimpleMotorFeedforward ff = new SimpleMotorFeedforward(0.1, 0.1, 0.1);
+
+  // Controllers
+  private final PIDController left = new PIDController(0.1, 0, 0.05);
+  private final PIDController right = new PIDController(0.1, 0, 0.05);
+
+  // Field
+  private final Field2d field = new Field2d();
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    m_rightMotor.setInverted(true);
+
+    odometry = new DifferentialDriveOdometry(new Rotation2d(m_gyro.getAngle()), m_leftEncoder.getDistance(),
+        m_rightEncoder.getDistance(), new Pose2d(0, 0, new Rotation2d()));
+
+    // Use inches as unit for encoder distances
+    m_leftEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
+    m_rightEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
+    resetEncoders();
+
+    SmartDashboard.putData(field);
+  }
+
+  public Command tankDrive(DoubleSupplier leftSpeed, DoubleSupplier rightSpeed) {
+    return run(() -> m_diffDrive.tankDrive(leftSpeed.getAsDouble(), rightSpeed.getAsDouble(), false));
+  }
+
+  // public Command setSpeeds(ChassisSpeeds chassisSpeeds) {
+  // DifferentialDriveWheelSpeeds wheelSpeeds = kinematics.toWheelSpeeds(chassisSpeeds);
+
+  //   new ChassisSpeeds()
+  // }
+
+  // public Command follow(PathPlannerTrajectory path) {
+
+  // }
+
+  // public Command followPath(PathPlannerTrajectory path, boolean resetOdometry)
+  // {
+
+  // }
+
+  public Pose2d getPose() {
+    return new Pose2d(m_leftEncoder.getDistance(), m_rightEncoder.getDistance(), new Rotation2d(m_gyro.getAngle()));
+  }
+
+  public void resetEncoders() {
+    m_leftEncoder.reset();
+    m_rightEncoder.reset();
+  }
+
+  public int getLeftEncoderCount() {
+    return m_leftEncoder.get();
+  }
+
+  public int getRightEncoderCount() {
+    return m_rightEncoder.get();
+  }
+
+  public double getLeftDistanceInch() {
+    return m_leftEncoder.getDistance();
+  }
+
+  public double getRightDistanceInch() {
+    return m_rightEncoder.getDistance();
+  }
+
+  public double getAverageDistanceInch() {
+    return (getLeftDistanceInch() + getRightDistanceInch()) / 2.0;
+  }
+
+  /**
+   * The acceleration in the X-axis.
+   *
+   * @return The acceleration of the XRP along the X-axis in Gs
+   */
+  public double getAccelX() {
+    return m_accelerometer.getX();
+  }
+
+  /**
+   * The acceleration in the Y-axis.
+   *
+   * @return The acceleration of the XRP along the Y-axis in Gs
+   */
+  public double getAccelY() {
+    return m_accelerometer.getY();
+  }
+
+  /**
+   * The acceleration in the Z-axis.
+   *
+   * @return The acceleration of the XRP along the Z-axis in Gs
+   */
+  public double getAccelZ() {
+    return m_accelerometer.getZ();
+  }
+
+  /**
+   * Current angle of the XRP around the X-axis.
+   *
+   * @return The current angle of the XRP in degrees
+   */
+  public double getGyroAngleX() {
+    return m_gyro.getAngleX();
+  }
+
+  /**
+   * Current angle of the XRP around the Y-axis.
+   *
+   * @return The current angle of the XRP in degrees
+   */
+  public double getGyroAngleY() {
+    return m_gyro.getAngleY();
+  }
+
+  /**
+   * Current angle of the XRP around the Z-axis.
+   *
+   * @return The current angle of the XRP in degrees
+   */
+  public double getGyroAngleZ() {
+    return m_gyro.getAngleZ();
+  }
+
+  /** Reset the gyro. */
+  public void resetGyro() {
+    m_gyro.reset();
+  }
+
+  public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
+    m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
+  }
+
+  @Override
+  public void periodic() {
+    odometry.update(new Rotation2d(m_gyro.getAngle()), m_leftEncoder.getDistance(), m_rightEncoder.getDistance());
+    field.setRobotPose(getPose());
+
+
+  }
+}
